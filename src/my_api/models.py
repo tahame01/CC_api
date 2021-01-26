7548567f16c4507a73506d7bcf0e51f66ced1dc5
tahame01 <tahame01@dcs.bbk.ac.uk>
2021-01-26 11:55:47 +0000

    models.py added

--- src/my_api/models.py
+++ src/my_api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Character(models.Model):
+    name = models.CharField(max_length=64)
+    location = models.CharField(max_length=64)
+    vehicle = models.charfield(max_length=64)
+    human = models.Booleanfield()
+    def __str__(self):
+        return self.name
