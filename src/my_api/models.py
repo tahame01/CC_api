803263885dd552e8baf9b6a467d501a5457e52b5
tahame01 <tahame01@dcs.bbk.ac.uk>
2021-01-26 12:11:42 +0000

    models.py corrected

--- src/my_api/models.py
+++ src/my_api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Character(models.Model):
     name = models.CharField(max_length=64)
     location = models.CharField(max_length=64)
-    vehicle = models.Charfield(max_length=64)
-    human = models.Booleanfield()
+    vehicle = models.CharField(max_length=64)
+    human = models.BooleanField()
     def __str__(self):
         return self.name
