d871b10d538f9cf73cdd4f7a1d4d08756619ebd7
tahame01 <tahame01@dcs.bbk.ac.uk>
2021-01-26 12:57:28 +0000

    lots of files added

--- src/my_api/views.py
+++ src/my_api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import CharacterSerializer
 
 # Create your views here.
+class CharacterViewSet(viewsets.ModelViewSet):
+    queryset = Character.objects.all().order_by('name')
+    serializer_class = CharacterSerializer
